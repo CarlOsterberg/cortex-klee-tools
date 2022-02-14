7177e9034e74cd3a2532050e4ee795511eee41ec
CarlOsterberg <carl.vilhelms.osterberg@gmail.com>
2022-02-14 15:31:38 +0100

    barebones llvmir to m4 cycles implementation, incorrect conversions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
+
+use llvmir_to_m4_cycles::IrToM4;
 mod ktest_parser;
+mod llvmir_to_m4_cycles;
 mod tests;
 
 //https://github.com/klee/klee/blob/master/tools/ktest-tool/ktest-tool
@@ -9,8 +12,8 @@ fn main() {
     dir.push("src");
     dir.push("ktest_parser");
     dir.push("test_cases");
-    dir.push("regexp");
-    let ktests = ktest_parser::read_instr(dir).unwrap();
+    dir.push("getsign");
+    let ktests = llvmir_to_m4_cycles::IrToM4::read_dir(dir);
     for ktest in ktests {
         println!("{:?}",ktest);
     }
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,11 @@
 use std::{fs,env};
 use crate::ktest_parser;
+use crate::llvmir_to_m4_cycles;
 
 #[cfg(test)]
 mod tests {
+    use crate::llvmir_to_m4_cycles;
+
     use super::*;
 
     #[test]
@@ -27,6 +30,19 @@ mod tests {
         let paths =fs::read_dir(dir);
         for test_folder in paths.unwrap() {
             let instructions = ktest_parser::read_instr(test_folder.unwrap().path());
+            assert!(instructions.is_ok());
+        }
+    }
+
+    #[test]
+    fn test_llvmir_to_m4_cycles_parsing() {
+        let mut dir = env::current_dir().unwrap();
+        dir.push("src");
+        dir.push("ktest_parser");
+        dir.push("test_cases");
+        let paths =fs::read_dir(dir);
+        for test_folder in paths.unwrap() {
+            let instructions = llvmir_to_m4_cycles::IrToM4::read_dir(test_folder.unwrap().path());
             assert!(instructions.is_ok());
         }
     }
